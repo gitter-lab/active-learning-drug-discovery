e2ab4e613a2a4eef7bb6c488fa8dae469f847ddc
Malnammi <alnammi2009@gmail.com>
2018-10-24 12:58:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Active Learning in Drug Discovery
+
+[![Build Status](https://travis-ci.org/gitter-lab/active-learning.svg?branch=master)](https://travis-ci.org/gitter-lab/active-learning)
+
+## TODO
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='active-learning',
+      version='0.1.0',
+      description='Active Learning in Drug Discovery',
+      url='https://github.com/gitter-lab/active-learning',
+      license='MIT',
+      packages=['active-learning'],
+      zip_safe=False)
