a3dd2ade0186164a03102d885861020dd81ef5ec
Malnammi <alnammi2009@gmail.com>
2020-07-01 15:35:37 -0500

    PstP update.
    - Update to pstp chtc runner.
    - Typo fix cummulative -> cumulative.

--- chtc_runners/experiment_PstP_runner.py
+++ chtc_runners/experiment_PstP_runner.py
@@ -3,13 +3,13 @@
     Experiment 4 - PstP: prospective screening of PstP target. True labels are unknown. 
     
     Usage:
-        python experiment_PstP_runner.py \
-        --pipeline_params_json_file=../param_configs/exp_PstP_pipeline_config.json \
-        --hyperparams_json_file=../param_configs/experiment_PstP_hyperparams/custom_cbws/ClusterBasedWCSelector_custom_1.json \
-        --iter_max=3 \ 
-        --batch_size_index=0 \
+        python experiment_pstp_runner.py \
+        --pipeline_params_json_file=../param_configs/exp_pstp_pipeline_config.json \
+        --hyperparams_json_file=../param_configs/experiment_pstp_hyperparams/custom_cbws/ClusterBasedWCSelector_custom_1.json \
+        --iter_num=0 \ 
+        --label_setting=adaptive \
         --no-precompute_dissimilarity_matrix \
-        [--initial_dataset_file=../datasets/PstP/diverse_plate.csv]
+        [--initial_dataset_file=../datasets/pstp/diverse_plate.csv]
 """
 
 from __future__ import absolute_import
@@ -38,8 +38,8 @@ if __name__ ==  '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pipeline_params_json_file', action="store", dest="pipeline_params_json_file", required=True)
     parser.add_argument('--hyperparams_json_file', action="store", dest="hyperparams_json_file", required=True)
-    parser.add_argument('--iter_max', type=int, default=10, action="store", dest="iter_max", required=False)
-    parser.add_argument('--batch_size_index', type=int, default=0, action="store", dest="batch_size_index", required=False)
+    parser.add_argument('--iter_num', type=int, default=0, action="store", dest="iter_num", required=True)
+    parser.add_argument('--label_setting', action="store", dest="label_setting", required=True)
     parser.add_argument('--precompute_dissimilarity_matrix', dest='precompute_dissimilarity_matrix', action='store_true')
     parser.add_argument('--no-precompute_dissimilarity_matrix', dest='precompute_dissimilarity_matrix', action='store_false')
     parser.add_argument('--initial_dataset_file', action="store", dest="initial_dataset_file", default=None, required=False)
@@ -48,10 +48,9 @@ if __name__ ==  '__main__':
     pipeline_params_json_file = given_args.pipeline_params_json_file
     hyperparams_json_file = given_args.hyperparams_json_file
     initial_dataset_file = given_args.initial_dataset_file
-    iter_max = given_args.iter_max
-    batch_size_index = given_args.batch_size_index
+    iter_num = given_args.iter_num
+    label_setting = given_args.label_setting
     precompute_dissimilarity_matrix = given_args.precompute_dissimilarity_matrix
-    start_iter = 0
     
     # load param json configs
     with open(pipeline_params_json_file) as f:
@@ -60,52 +59,36 @@ if __name__ ==  '__main__':
         next_batch_selector_params = json.load(f)
     
     # setup initial training plate
-    pathlib.Path(pipeline_config['training_data_params']['data_path_format']).parent.mkdir(parents=True, exist_ok=True)
-    [os.remove(f) for f in glob.glob(pipeline_config['training_data_params']['data_path_format'].format('*'))]
-    
+    pathlib.Path(pipeline_config['training_data_params']['data_path_format'].format(label_setting, 0)).parent.mkdir(parents=True, exist_ok=True)
+        
     if initial_dataset_file is not None:
-        shutil.copyfile(initial_dataset_file, pipeline_config['training_data_params']['data_path_format'].format(0))
+        shutil.copyfile(initial_dataset_file, pipeline_config['training_data_params']['data_path_format'].format(label_setting, 0))
         assert pd.read_csv(initial_dataset_file).equals(pd.read_csv(pipeline_config['training_data_params']['data_path_format'].format(0)))
     else:
         initial_dataset_file = 'None' 
         
     print('Set {} as starting initial set.'.format(initial_dataset_file))
-    params_set_results_dir = pipeline_config['common']['params_set_results_dir'].format(next_batch_selector_params['hyperparameter_group'],
-                                                                                        next_batch_selector_params['hyperparameter_id'],
-                                                                                        initial_dataset_file)
-    params_set_config_csv = params_set_results_dir+'/'+pipeline_config['common']['params_set_config_csv']
-    pathlib.Path(params_set_config_csv).parent.mkdir(parents=True, exist_ok=True)
-    rnd_seed = next_batch_selector_params['rnd_seed']
-    with open(params_set_config_csv,'w') as f:
-        csv_w = csv.writer(f)
-        csv_w.writerow(list(next_batch_selector_params.keys()) + ['rnd_seed'])
-        csv_w.writerow(list(next_batch_selector_params.values()) + [rnd_seed])
-    # run this param set for each batch size
-    batch_size_list = next_batch_selector_params["batch_size"]
-    batch_size = batch_size_list[batch_size_index]
+    
+    # confirm cumulative_${iter_num}.csv.gz exists
+    current_train_csv = pipeline_config['training_data_params']['data_path_format'].format(label_setting, iter_num)
+    pipeline_config['training_data_params']['data_path_format'] = current_train_csv
+    if (iter_num > 0) or (iter_num == 0 and initial_dataset_file != 'None'):
+        assert os.path.exists(current_train_csv)
+    
+    next_batch_csv = pipeline_config['common']['next_batch_csv'].format(label_setting, iter_num)
+    
+    # set target/task name
+    if label_setting == "adaptive":
+        pipeline_config['common']['task_names'] = pipeline_config['common']['task_names'].format("Adaptive")
+    else:
+        pipeline_config['common']['task_names'] = pipeline_config['common']['task_names'].format("True")
+    
+    # get batch size
+    batch_size = pipeline_config['common']['batch_size']
+    next_batch_selector_params["batch_size"] = batch_size
     print('---------------------------------------------------------------')
     print('Starting AL pipeline with batch_size: {}'.format(batch_size))
-    next_batch_selector_params["batch_size"] = batch_size
-    batch_size_results_dir = params_set_results_dir + pipeline_config['common']['batch_size_results_dir'].format(batch_size)
     
-    if not os.path.exists(batch_size_results_dir):
-        pathlib.Path(batch_size_results_dir+'/'+pipeline_config['common']['params_set_config_csv']).parent.mkdir(parents=True, exist_ok=True)
-    
-    start_iter = len(glob.glob(batch_size_results_dir + '/training_data/iter_*')) - 1    
-    # modify location of training data to be able to continue jobs
-    if not os.path.exists(batch_size_results_dir + '/training_data/'):
-        import shutil
-        shutil.copytree(pathlib.Path(pipeline_config['training_data_params']['data_path_format']).parent, 
-                        batch_size_results_dir + '/training_data')
-        start_iter = 0
-        
-    pipeline_config['training_data_params']['data_path_format'] = batch_size_results_dir + '/training_data/iter_{}.csv'
-        
-    with open(batch_size_results_dir+'/'+pipeline_config['common']['params_set_config_csv'],'w') as f:
-        csv_w = csv.writer(f)
-        csv_w.writerow(list(next_batch_selector_params.keys()) + ['rnd_seed'])
-        csv_w.writerow(list(next_batch_selector_params.values()) + [rnd_seed])
-        
     try: 
         pipeline_config['common']['dissimilarity_memmap_filename']
     except:
@@ -117,31 +100,29 @@ if __name__ ==  '__main__':
         compute_dissimilarity_matrix(csv_file_or_dir=pipeline_config['unlabeled_data_params']['data_path_format'], 
                                      output_dir=pipeline_config['common']['dissimilarity_memmap_filename'])
     
-    # run iterations for this simulation
-    for iter_num in range(start_iter, min(start_iter+5, iter_max)):
-        iter_start_time = time.time()
-        print('---------------------------------------------------------------')
-        print('Processing iteration number: {}...'.format(iter_num))
-        #### Run single iteration of active learning pipeline ####
-        selection_start_time = time.time()
-        exploitation_df, exploration_df, exploitation_array, exploration_array = get_next_batch(training_loader_params=pipeline_config['training_data_params'], 
-                                                                                                unlabeled_loader_params=pipeline_config['unlabeled_data_params'],
-                                                                                                model_params=pipeline_config['model'],
-                                                                                                task_names=pipeline_config['common']['task_names'],
-                                                                                                next_batch_selector_params=next_batch_selector_params,
-                                                                                                dissimilarity_memmap_filename=pipeline_config['common']['dissimilarity_memmap_filename'])
-        selection_end_time = time.time()
-        total_selection_time = selection_end_time - selection_start_time
-        #### Evaluation: no evaluation since labels are unknown. Only save selection. ####
-        # save results
-        if exploitation_df is not None or exploration_df is not None:
-            # finally save the exploitation, exploration dataframes to training data directory for next iteration
-            pd.concat([exploitation_df, exploration_df]).to_csv(pipeline_config['training_data_params']['data_path_format'].format(iter_num+1),
-                                                                index=False)
-        iter_end_time = time.time()
-        print('Finished processing iteration {}. Took {} seconds.'.format(iter_num, iter_end_time-iter_start_time))
+    # run current iter for this simulation
+    iter_start_time = time.time()
+    print('---------------------------------------------------------------')
+    print('Processing iteration number: {}...'.format(iter_num))
+    #### Run single iteration of active learning pipeline ####
+    selection_start_time = time.time()
+    exploitation_df, exploration_df, exploitation_array, exploration_array = get_next_batch(training_loader_params=pipeline_config['training_data_params'], 
+                                                                                            unlabeled_loader_params=pipeline_config['unlabeled_data_params'],
+                                                                                            model_params=pipeline_config['model'],
+                                                                                            task_names=pipeline_config['common']['task_names'],
+                                                                                            next_batch_selector_params=next_batch_selector_params,
+                                                                                            dissimilarity_memmap_filename=pipeline_config['common']['dissimilarity_memmap_filename'])
+    selection_end_time = time.time()
+    total_selection_time = selection_end_time - selection_start_time
+    #### Evaluation: no evaluation since labels are unknown. Only save selection. ####
+    # save results
+    if exploitation_df is not None or exploration_df is not None:
+        # finally save the exploitation, exploration dataframes to training data directory for next iteration
+        pd.concat([exploitation_df, exploration_df]).to_csv(next_batch_csv, compression='gzip',
+                                                            index=False)
+    iter_end_time = time.time()
+    print('Finished processing iteration {}. Took {} seconds.'.format(iter_num, iter_end_time-iter_start_time))
 
-        # terminate if both exploitation and exploration df are None
-        if exploitation_df is None and exploration_df is None:
-            print('Both exploitation and exploration selections are empty. Terminating program.')
-            break
+    # terminate if both exploitation and exploration df are None
+    if exploitation_df is None and exploration_df is None:
+        print('Both exploitation and exploration selections are empty. Terminating program.')
